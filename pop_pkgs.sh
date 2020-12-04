6bd205dc2b4512b713dc9ef2a5d5529946333202
Mike Carter <carterjmike@gmail.com>
2020-12-04 00:25:55 -0500

    Update pop_pkgs.sh

--- pop_pkgs.sh
+++ pop_pkgs.sh
@@ -35,10 +35,10 @@ case $input in
 esac
 
 # Setup for current version of R
-#echo
-#echo "Adding keys for current version of R"
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-#sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+echo
+echo "Adding keys for current version of R"
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu groovy-cran40/'
 
 # Update package lists
 echo
